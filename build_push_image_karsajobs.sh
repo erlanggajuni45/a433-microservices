60e4396e4a587b0d2f5687754a22d61c7d5d394c
Erlangga Juni Saputra <erlanggajuni45@gmail.com>
2026-05-10 01:28:49 +0700

    menambahkan file sh untuk build dan push image ke ghcr

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,8 @@
+# membuat docker image untuk karsajobs dari Dockerfile yang ada di folder karsajobs
+docker build -t ghcr.io/erlanggajuni45/karsajobs:latest .
+
+# login ke Github Container Registry
+echo $GITHUB_TOKEN | docker login ghcr.io -u erlanggajuni45 --password-stdin
+
+# push image ke Github Container Registry
+docker push ghcr.io/erlanggajuni45/karsajobs:latest
